1fd99d489af6c8fca06fbe00fe2e8f96d3e8ce11
Ian Dunn <ian@iandunn.name>
2012-09-15 12:51:59 -0700

    Wrapping in if( class_exists() )

--- tests/unit/wpps-unit-test-suite.php
+++ tests/unit/wpps-unit-test-suite.php
@@ -8,32 +8,35 @@
  * @author Ian Dunn <ian@iandunn.name>
  * @link http://wordpress.org/extend/plugins/simpletest-for-wordpress/
  */
-class WPPSUnitTestSuite extends TestSuite
+if( !class_exists( 'WPPSUnitTestSuite' ) )
 {
-	function __construct()
+	class WPPSUnitTestSuite extends TestSuite
 	{
-		parent::__construct();
-
-		$this->addFile( dirname( __FILE__ ) . '/unit-test-wpps-settings.php' );
-		$this->addFile( dirname( __FILE__ ) . '/unit-test-wpps-non-static-class.php' );
-   }
+		function __construct()
+		{
+			parent::__construct();
 	
-	/**
-	 * Sets a protected or private method to be accessible
-	 * @author Joel Uckelman <http://www.nomic.net/~uckelman/>
-	 * @link http://stackoverflow.com/questions/249664/best-practices-to-test-protected-methods-with-phpunit
-	 * @param string $className
-	 * @param string $methodName
-	 * @return object
-	 */
-	public static function getHiddenMethod( $className, $methodName )
-	{
-		$class = new ReflectionClass( $className );
-		$method = $class->getMethod( $methodName );
-		$method->setAccessible( true );
+			$this->addFile( dirname( __FILE__ ) . '/unit-test-wpps-settings.php' );
+			$this->addFile( dirname( __FILE__ ) . '/unit-test-wpps-non-static-class.php' );
+	   }
 		
-		return $method;
-	}
+		/**
+		 * Sets a protected or private method to be accessible
+		 * @author Joel Uckelman <http://www.nomic.net/~uckelman/>
+		 * @link http://stackoverflow.com/questions/249664/best-practices-to-test-protected-methods-with-phpunit
+		 * @param string $className
+		 * @param string $methodName
+		 * @return object
+		 */
+		public static function getHiddenMethod( $className, $methodName )
+		{
+			$class = new ReflectionClass( $className );
+			$method = $class->getMethod( $methodName );
+			$method->setAccessible( true );
+			
+			return $method;
+		}
+	} // end WPPSUnitTestSuite
 }
 
 ?>
