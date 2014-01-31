8b0add594e3a59ad229d311c93e0236dd7f75ceb
Ian Dunn <ian@iandunn.name>
2014-01-31 11:46:51 -0800

    textarea tags should not self-close

--- views/wpps-settings/page-settings-fields.php
+++ views/wpps-settings/page-settings-fields.php
@@ -20,7 +20,7 @@
 
 <?php if ( WordPressPluginSkeleton::PREFIX . 'field-example2' == $field['label_for'] ) : ?>
 
-	<textarea id="<?php esc_attr_e( WordPressPluginSkeleton::PREFIX . 'settings[advanced][field-example2]' ); ?>" name="<?php esc_attr_e( WordPressPluginSkeleton::PREFIX . 'settings[advanced][field-example2]' ); ?>" class="large-text" /><?php echo esc_textarea( $this->settings['advanced']['field-example2'] ); ?></textarea>
+	<textarea id="<?php esc_attr_e( WordPressPluginSkeleton::PREFIX . 'settings[advanced][field-example2]' ); ?>" name="<?php esc_attr_e( WordPressPluginSkeleton::PREFIX . 'settings[advanced][field-example2]' ); ?>" class="large-text"><?php echo esc_textarea( $this->settings['advanced']['field-example2'] ); ?></textarea>
 	<p class="description">This is an example of a longer explanation.</p>
 
 <?php elseif ( WordPressPluginSkeleton::PREFIX . 'field-example3' == $field['label_for'] ) : ?>
